0ec25c41a1fc5aaffb121a9c2fd8fa25f8e24663
eaostrom <75504916+eaostrom@users.noreply.github.com>
2020-12-08 13:38:53 -0500

    Update problem2.py
    Updated to use a while loop rather than a for loop, as it is cleaner and smoother.

--- problem2.py
+++ problem2.py
@@ -2,15 +2,13 @@
 #First several terms: 1,2,3,5,8,13,34,55,89
 #Find sum of even-valued terms under 4 million (inclusive)
 
-fibonacci=[1,2]
-for x in range(100000):
-    x= fibonacci[x]+fibonacci[x+1]
-    fibonacci.append(x)
- #this range is arbitrary. Hopefully it includes all values under 4 million.
-
 result= []
-for index_a,a in enumerate(fibonacci):
-    if a <= 4000000:
-        if a%2==0:
-            result.append(a)
+prev = 1
+current = 2
+while current <= 4000000:
+    next = prev + current
+    prev = current
+    current = next
+    if prev%2==0:
+        result.append(prev)
 print(sum(result))
